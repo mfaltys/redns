21933ccca824742f4c264a64efe8ed5f210f5527
Matthew Faltys <mfaltys@gmail.com>
2017-02-25 14:48:52 -0600

    Add incorrect test case

--- doic/upstream_query_test.go
+++ doic/upstream_query_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/miekg/dns"
 )
 
-func TestANameResolve(t *testing.T) {
+func TestANameResolveCorrect(t *testing.T) {
 	// read in conf up a directory
 	readConf("../deps/testing.config.gcfg")
 
@@ -20,8 +20,38 @@ func TestANameResolve(t *testing.T) {
 		t.Error("error sending request. is the server running?")
 	}
 
-	// print response
-	// TODO: logic behind response answer
-	//t.Log(req.Question[0])
-	t.Log(req.Answer[0])
+	// print header
+	t.Log(req.MsgHdr.String())
+
+	response := fmt.Sprintf("%s", dns.RcodeToString[req.MsgHdr.Rcode])
+	if response == "NOERROR" {
+		t.Log(req.Answer[0])
+		t.Log("\x1b[31mrecieved NOERROR successfully\x1b[39m")
+	} else {
+		t.Errorf("expected 'NOERROR', got %v instead", response)
+	}
+}
+
+func TestANameResolveInorrect(t *testing.T) {
+	// read in conf up a directory
+	readConf("../deps/testing.config.gcfg")
+
+	m1 := new(dns.Msg)
+	m1.SetQuestion(dns.Fqdn("not.a.domain"), dns.TypeA)
+
+	addr := fmt.Sprintf("127.0.0.1:%d", config.Doic.DNSPort)
+	req, err := dns.Exchange(m1, addr)
+	if err != nil {
+		t.Error("error sending request. is the server running?")
+	}
+
+	// print header
+	t.Log(req.MsgHdr.String())
+
+	response := fmt.Sprintf("%s", dns.RcodeToString[req.MsgHdr.Rcode])
+	if response == "NXDOMAIN" {
+		t.Log("\x1b[31mrecieved NXDOMAIN successfully\x1b[39m")
+	} else {
+		t.Errorf("expected 'NXDOMAIN', got %v instead", response)
+	}
 }
