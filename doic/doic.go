c1e779c825b704b0ab1a85ed00ead39bb74b2215
Matthew Faltys <mfaltys@gmail.com>
2017-02-24 21:50:06 -0600

    Add config init

--- doic/doic.go
+++ doic/doic.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"gopkg.in/gcfg.v1"
+)
+
+type Config struct {
+	Doic struct {
+		Loglevel string
+	}
+
+	Redis struct {
+		Host     string
+		Password string
+	}
+}
+
+var config = Config{}
+
 func main() {
+	readConf()
 	println("hello world")
+}
+
+func readConf() {
+	// init the config
+	err := gcfg.ReadFileInto(&config, "config.gcfg")
+	if err != nil {
+		panic(fmt.Sprintf("could not load config.gcfg, error: %s\n", err))
+	}
 }
