5af2eb2d42327ef028715a4a3ebefc02d36f1fff
Matthew Faltys <mfaltys@gmail.com>
2017-04-02 14:00:58 -0500

    Add initial blacklist validation

--- doic/upstream_query.go
+++ doic/upstream_query.go
@@ -11,11 +11,31 @@ import (
 	"gopkg.in/redis.v5"
 )
 
-func upstreamQuery(w dns.ResponseWriter, req *dns.Msg) *dns.Msg {
-	// TODO
-	//   - check hostname against malicious domain list
-	//   - check ip against malicious domain list
-	//   - handler when malicious address/domain is detected
+func upstreamQuery(w dns.ResponseWriter, req *dns.Msg, redisClient *redis.Client) *dns.Msg {
+	// parse hostname
+	hostname := req.Question[0].Name
+
+	// parse client ip
+	client := strings.Split(w.RemoteAddr().String(), ":")
+
+	// un-fully qualify domain if its qualified
+	hostLen := len(hostname)
+	if hostLen > 0 && hostname[hostLen-1] == '.' {
+		hostname = hostname[:hostLen-1]
+	}
+
+	// query redis to see if entry exists in 'blacklist:domain' O(1)
+	exists, err := redisClient.SIsMember("blacklist:domain", hostname).Result()
+	if err != nil {
+		glogger.Error.Println("error getting result from blacklist:domain")
+		glogger.Error.Println(err)
+	}
+
+	// handle blacklisted domain case
+	if exists {
+		// TODO add 'nonexistent' response to client
+		glogger.Debug.Printf("intercepted blacklisted domain '%s' on client '%s'\n", hostname, client[0])
+	}
 
 	transport := "udp"
 	if _, ok := w.RemoteAddr().(*net.TCPAddr); ok {
@@ -32,8 +52,13 @@ func upstreamQuery(w dns.ResponseWriter, req *dns.Msg) *dns.Msg {
 }
 
 func anameresolve(w dns.ResponseWriter, req *dns.Msg, redisClient *redis.Client) {
+	// parse hostname
 	hostname := req.Question[0].Name
+
+	// parse client ip
 	client := strings.Split(w.RemoteAddr().String(), ":")
+
+	// generate timestamp
 	t := time.Now()
 	timestamp := fmt.Sprintf("%s", t.Format("2006/01/02 15:04:05"))
 	//timestamp := fmt.Sprintf("%s", t.Format(time.RFC1123))
@@ -44,6 +69,7 @@ func anameresolve(w dns.ResponseWriter, req *dns.Msg, redisClient *redis.Client)
 		glogger.Error.Printf("error adding client: '%s' to 'client:list'", client[0])
 	}
 
+	// add client history entry to redis
 	err = redisClient.RPush(fmt.Sprintf("client:%s", client[0]), fmt.Sprintf("%s :: %s", timestamp, hostname)).Err()
 	if err != nil {
 		glogger.Error.Printf("error adding hostname: '%s' for client: 'client:%s'\n", hostname, client)
@@ -53,6 +79,7 @@ func anameresolve(w dns.ResponseWriter, req *dns.Msg, redisClient *redis.Client)
 	// send request upstream
 	glogger.Debug.Printf("client: %s\n", client[0])
 	glogger.Debug.Printf("sending request for '%s' upstream\n", hostname)
-	req = upstreamQuery(w, req)
+	req = upstreamQuery(w, req, redisClient)
+	// write response back from client
 	w.WriteMsg(req)
 }
